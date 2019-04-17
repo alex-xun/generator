1fda950ebd0b8ab66156202ff6a5e2b9ea00814d
xunzhibin <xunzhibin>
2019-04-17 15:51:33 +0800

    添加test

---
+++ src/Tools/Tools.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * 简述区
+ *
+ * 详细说明区
+ *
+ * @author    Alex Xun xunzhibin@expert.com
+ * @version   1.0
+ * @copyright (C) 2019 Jnexpert Ltd. All rights reserved
+ * @file      Tools.php
+ */
+
+namespace Tools;
+
+class Tools
+{
+    public function __construct()
+    {
+    }
+
+    public static function hi()
+    {
+        echo 'he';
+    }
+}
+
